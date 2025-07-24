5275defbc18fd79a0829c6fcacbf515cfcbd3314
Ramanpreet Aujla <ramanpreetaujla@Ramanpreets-Mac-mini.local>
2025-07-24 16:17:48 -0400

    handling errors and showing synonyms

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { useState } from "react";
+import React, { useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,34 +12,87 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const [word, setWord] = useState<string>('');
+  const [error, setError] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean | undefined>(undefined);
   const [synonymWordList, setSynonymWordList] = useState<Array<string>>([]);
 
-  const findSynonym = async (formData: FormData) => {
-    const word = formData.get('word') as string | null;
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError('');
 
     if (word != null && word.trim().length > 0) {
-      const response = await fetch(`https://api.datamuse.com/words?rel_syn=${word}`)
+      const response = await fetch(`https://api.datamuse.com/words?rel_syn=${word}`);
 
       const wordList: Array<string> = (await response.json() as Array<{ word: string; score: number }>).map((datamuseResponse => {
         return datamuseResponse.word
       }));
 
       setSynonymWordList(wordList);
+      setIsLoading(false)
+    } else {
+      setError('Enter a valid word to search')
     }
-
   }
 
   return (
     <section className="synonym-finder">
-      <form action={findSynonym} className="form">
-        <label htmlFor="word" className="label">
+      <form className="form" onSubmit={(e) => handleFormSubmit(e)}>
+        <label htmlFor="word-input" className="label">
           Enter the word to find a synonym:
         </label>
-        <input type="text" className="word" name="word" placeholder="Enter the word to find a synonym:" />
+        <input
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          type="text"
+          id="word-input"
+          className="word"
+          name="word"
+          placeholder="Enter the word to find a synonym:"
+        />
+        {
+          error && (
+            <span>There is an error</span>
+          )
+        }
         <button className="submit">
           Find
         </button>
       </form>
+
+      {
+        !error && isLoading != null && isLoading && (
+          <div className="loader">
+            Loading...
+          </div>
+        )
+      }
+
+      {
+        !error && isLoading != null && !isLoading && (
+          <ul>
+            Synonym List
+            {
+              synonymWordList.length === 0 && (
+                <>
+                  <br />
+                  <span>No Synonyms found</span>
+                </>
+              )
+            }
+            {synonymWordList.map((synonym) => {
+              return (
+                <li>
+                  {synonym}
+                </li>
+              )
+            })}
+          </ul>
+        )
+      }
+
+
     </section>
   );
 }
